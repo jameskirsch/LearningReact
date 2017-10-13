64ccc7607728f05b7d7f23d3e792327e3df39040
jameskirsch <jjamesdk@yahoo.com>
2017-10-13 13:22:16 -0400

    Installing babel cli, fixing package.json to support a react preset with babel, also move jsx into separate file to be transpiled from

--- index.jsx
+++ index.jsx
@@ -1,1 +1,14 @@
-console.log('testit');
+class HelloWorld extends React.Component {
+    render() {
+       return(
+         <div>
+            <h1>1. Showing from transpiled JSX</h1>
+            <h1>2. Showing from transpiled JSX</h1>
+        </div>
+       )
+    }
+}
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById('content')
+)
