7bd47053e1c5512b99fb8c0ecebb0008993105be
jameskirsch <jjamesdk@yahoo.com>
2017-10-14 17:58:52 -0400

    figuring out attributes in react

--- index.jsx
+++ index.jsx
@@ -1,7 +1,39 @@
+let thisReactElement = <h1>TestMyJsx</h1>
+class MyJsxTestClass extends React.Component {
+    render() {
+        return <div>
+            {thisReactElement}
+            {thisReactElement}
+        </div> 
+    }
+}
+ReactDOM.render(
+    <MyJsxTestClass/>,
+    document.getElementById('jsxStuff')
+)
+
+class ProfileLink extends React.Component {
+    render() {
+        return (
+            <a href={this.props.url}
+               title={this.props.label}
+               target = "_blank" >Profile
+            </a>
+        )
+    }
+}
+
+ReactDOM.render(
+    <ProfileLink/>,
+    document.getElementById('profile')
+)
+
+
 class HelloWorld extends React.Component {
     render() {
        return(
-         <div>
+         <div id ="ohNo">
+            <a href="http://google.com">Google</a>
             <h1>1. Showing from transpiled JSX</h1>
             <h1>2. Showing from transpiled JSX</h1>
         </div>
@@ -11,4 +43,18 @@ class HelloWorld extends React.Component {
 ReactDOM.render(
     <HelloWorld/>,
     document.getElementById('content')
+)
+class DateTimeNow extends React.Component {
+    render() {
+        let dateTimeNow = new Date().toLocaleString()
+        return React.createElement(
+            'span',
+            null,
+            'current' + dateTimeNow
+        )
+    }
+}
+ReactDOM.render(
+    <DateTimeNow/>,
+    document.getElementById('dateTime')
 )
--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-class HelloWorld extends React.Component {
+let thisReactElement = React.createElement(
+    'h1',
+    null,
+    'TestMyJsx'
+);
+class MyJsxTestClass extends React.Component {
     render() {
         return React.createElement(
             'div',
             null,
+            thisReactElement,
+            thisReactElement
+        );
+    }
+}
+ReactDOM.render(React.createElement(MyJsxTestClass, null), document.getElementById('jsxStuff'));
+
+class ProfileLink extends React.Component {
+    render() {
+        return React.createElement(
+            'a',
+            { href: this.props.url,
+                title: this.props.label,
+                target: '_blank' },
+            'Profile'
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(ProfileLink, null), document.getElementById('profile'));
+
+class HelloWorld extends React.Component {
+    render() {
+        return React.createElement(
+            'div',
+            { id: 'ohNo' },
+            React.createElement(
+                'a',
+                { href: 'http://google.com' },
+                'Google'
+            ),
             React.createElement(
                 'h1',
                 null,
@@ -17,3 +53,10 @@ class HelloWorld extends React.Component {
     }
 }
 ReactDOM.render(React.createElement(HelloWorld, null), document.getElementById('content'));
+class DateTimeNow extends React.Component {
+    render() {
+        let dateTimeNow = new Date().toLocaleString();
+        return React.createElement('span', null, 'current' + dateTimeNow);
+    }
+}
+ReactDOM.render(React.createElement(DateTimeNow, null), document.getElementById('dateTime'));
